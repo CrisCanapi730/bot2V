7792f568e9af4e1b47e028647451b21748e15475
“Cristian <cristiancanapi730@gmail.com>
2023-08-14 22:36:40 -0400

    Bot saluda por nombre

--- src/presenter.js
+++ src/presenter.js
@@ -1,1 +1,13 @@
+import saludar from "./saludador";
 
+const nombreIn = document.querySelector("#nombre");
+const formSaludo = document.querySelector("#saludo-form");
+const saludoDiv = document.querySelector("#saludo-div");
+
+formSaludo.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const nombre = nombreIn.value;
+
+  saludoDiv.innerHTML = "<p>" + saludar(nombre) + "</p>";
+})
