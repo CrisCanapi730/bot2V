d58b2e2153bbb27d78be5370049d26181c32f4fe
“Cristian <cristiancanapi730@gmail.com>
2023-08-15 02:43:51 -0400

    Version final del bot con idioma implementada

--- src/presenter.js
+++ src/presenter.js
@@ -6,6 +6,15 @@ const saludoDiv = document.querySelector("#saludo-div");
 const generoSelec = document.querySelector("#genero");
 const edadIn = document.querySelector("#edad");
 
+
+const esButton = document.createElement("button");
+esButton.textContent = "es";
+document.body.appendChild(esButton);
+
+const enButton = document.createElement("button");
+enButton.textContent = "en";
+document.body.appendChild(enButton);
+
 formSaludo.addEventListener("submit", (event) => {
   event.preventDefault();
 
@@ -15,6 +24,34 @@ formSaludo.addEventListener("submit", (event) => {
   let fechaActual = new Date();
   let horaActual = fechaActual.getHours();
 
+  saludoDiv.innerHTML =
+    "<p>" + saludar(nombre, sexo, edad, horaActual, selectedLanguage) + "</p>";
+});
 
-  saludoDiv.innerHTML = "<p>" + saludar(nombre, sexo, edad, horaActual) + "</p>";
-})
+
+let selectedLanguage = "es";
+
+esButton.addEventListener("click", () => {
+  selectedLanguage = "es";
+  updateContent(selectedLanguage);
+});
+
+enButton.addEventListener("click", () => {
+  selectedLanguage = "en";
+  updateContent(selectedLanguage);
+});
+
+
+function updateContent() {
+  // Update the text of the HTML elements on your page
+  document.querySelector("h1").textContent = selectedLanguage === 'es' ? 'Bot Saludador' : 'Greetings Bot';
+  document.querySelector("label[for='nombre']").textContent = selectedLanguage === 'es' ? 'Nombre: ' : 'Name: ';
+  document.querySelector("label[for='edad']").textContent = selectedLanguage === 'es' ? 'Edad: ' : 'Age: ';
+  document.querySelector("label[for='genero']").textContent = selectedLanguage === 'es' ? 'Género: ' : 'Gender: ';
+  document.querySelector("#genero option[value='']").textContent = selectedLanguage === 'es' ? 'Selecciona tu género' : 'Select your gender';
+  document.querySelector("#genero option[value='masculino']").textContent = selectedLanguage === 'es' ? 'Masculino' : 'Male';
+  document.querySelector("#genero option[value='femenino']").textContent = selectedLanguage === 'es' ? 'Femenino' : 'Female';
+  document.querySelector("button[type='submit']").textContent = selectedLanguage === 'es' ? 'Saludar' : 'Greet';
+}
+
+
--- src/saludador.js
+++ src/saludador.js
@@ -1,53 +1,71 @@
-
-function saludar(nombre, sexo, edad, hora){
-    if(hora >= 0 && hora<12)
-    {
-        if(sexo === 'masculino' && edad>30)
-        {
-            return "Buenos dias señor " + nombre;
-        }
-        else if(sexo === 'masculino' && edad<=30)
-        {
-            return "Buenos dias joven " + nombre;
-        }
-        else if(sexo === 'femenino' && edad>30)
-        {
-            return "Buenos dias señora " + nombre;
-        }
-        else if(sexo === 'femenino' && edad<=30)
-        {
-            return "Buenos dias señorita " + nombre;
-        }
-        else
-        {
-            return "Buenos dias " + nombre;
-        }
+function saludar(nombre, sexo, edad, hora, idioma) {
+    let saludo = "";
+    let mensajes = {
+      es: {
+        masculine: {
+          old: "Buenos dias señor ",
+          young: "Buenos dias joven "
+        },
+        feminine: {
+          old: "Buenos dias señora ",
+          young: "Buenos dias señorita "
+        },
+        neutral: "Buenos dias "
+      },
+      en: {
+        masculine: {
+          old: "Good morning sir ",
+          young: "Good morning young "
+        },
+        feminine: {
+          old: "Good morning madam ",
+          young: "Good morning miss "
+        },
+        neutral: "Good morning "
+      }
+    };
+  
+    if (hora >= 0 && hora < 12) {
+      if (sexo === "masculino" && edad > 30) {
+        saludo = mensajes[idioma].masculine.old + nombre;
+      } else if (sexo === "masculino" && edad <= 30) {
+        saludo = mensajes[idioma].masculine.young + nombre;
+      } else if (sexo === "femenino" && edad > 30) {
+        saludo = mensajes[idioma].feminine.old + nombre;
+      } else if (sexo === "femenino" && edad <= 30) {
+        saludo = mensajes[idioma].feminine.young + nombre;
+      } else {
+        saludo = mensajes[idioma].neutral + nombre;
+      }
+    } else if (hora >= 12 && hora < 18) {
+      // Afternoon
+      if (sexo === "masculino" && edad > 30) {
+        saludo = idioma === 'es' ? 'Buenas tardes señor ' : 'Good afternoon sir ' + nombre;
+      } else if (sexo === "masculino" && edad <= 30) {
+        saludo = idioma === 'es' ? 'Buenas tardes joven ' : 'Good afternoon young ' + nombre;
+      } else if (sexo === "femenino" && edad > 30) {
+        saludo = idioma === 'es' ? 'Buenas tardes señora ' : 'Good afternoon madam ' + nombre;
+      } else if (sexo === "femenino" && edad <= 30) {
+        saludo = idioma === 'es' ? 'Buenas tardes señorita ' : 'Good afternoon miss ' + nombre;
+      } else {
+        saludo = idioma === 'es' ? 'Buenas tardes ': 'Good afternoon '+ nombre;
+      }
+    } else {
+      if (sexo === "masculino" && edad > 30) {
+        saludo = idioma === 'es' ? 'Buenas noches señor ': 'Good evening sir '+ nombre;
+      } else if (sexo === "masculino" && edad <= 30) {
+        saludo = idioma === 'es' ? 'Buenas noches joven ': 'Good evening young '+ nombre;
+      } else if (sexo === "femenino" && edad > 30) {
+        saludo = idioma === 'es' ? 'Buenas noches señora ': 'Good evening madam '+ nombre;
+      } else if (sexo === "femenino" && edad <= 30) {
+        saludo = idioma === 'es' ? 'Buenas noches señorita ': 'Good evening miss '+ nombre;
+      } else {
+        saludo = idioma === 'es' ? 'Buenas noches ': 'Good evening '+ nombre;
+      }
     }
-    else
-    {
-        if(sexo === 'masculino' && edad>30)
-        {
-            return "Buenos tardes señor " + nombre;
-        }
-        else if(sexo === 'masculino' && edad<=30)
-        {
-            return "Buenos tardes joven " + nombre;
-        }
-        else if(sexo === 'femenino' && edad>30)
-        {
-            return "Buenos tardes señora " + nombre;
-        }
-        else if(sexo === 'femenino' && edad<=30)
-        {
-            return "Buenos tardes señorita " + nombre;
-        }
-        else
-        {
-            return "Buenos tardes " + nombre;
-        }
-    }
-    
-        
-}
+  
+    return saludo;
+  }
+  
 
 export default saludar;
